c1d9e775af86fdaf810e5637689098dd787b7f31
Muhammed Kara <n.muhammed.kara@gmail.com>
2024-08-09 18:56:09 +0200

    add TestDeleteAccount

--- cmd/db/repositories/account_test.go
+++ cmd/db/repositories/account_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 )
@@ -101,4 +102,24 @@ func (suite *AccountTestSuite) TestUpdateAccount() {
 	require.Equal(suite.T(), arg2.Balance, account2.Balance)
 	require.Equal(suite.T(), account1.Currency, account2.Currency)
 	require.WithinDuration(suite.T(), account1.CreatedAt, account2.CreatedAt, time.Second)
+}
+
+func (suite *AccountTestSuite) TestDeleteAccount() {
+	arg := CreateAccountParams{
+		Owner:    "Max",
+		Balance:  100,
+		Currency: "EUR",
+	}
+
+	account1, err := testQueries.CreateAccount(suite.ctx, arg)
+	require.NoError(suite.T(), err)
+	require.NotEmpty(suite.T(), account1)
+
+	err = testQueries.DeleteAccount(suite.ctx, account1.ID)
+	require.NoError(suite.T(), err)
+
+	account2, err := testQueries.GetAccount(suite.ctx, account1.ID)
+	require.Error(suite.T(), err)
+	require.EqualError(suite.T(), err, pgx.ErrNoRows.Error())
+	require.Empty(suite.T(), account2)
 }
