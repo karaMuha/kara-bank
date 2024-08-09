d616afce11adb33b6effd1dd9d1e6d8c5e1e6d05
Muhammed Kara <n.muhammed.kara@gmail.com>
2024-08-09 18:50:38 +0200

    add TestUpdateAccount and implement test suite for account tests

--- cmd/db/repositories/account_test.go
+++ cmd/db/repositories/account_test.go
@@ -6,48 +6,99 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestCreateAccount(t *testing.T) {
+type AccountTestSuite struct {
+	suite.Suite
+	ctx context.Context
+}
+
+func TestAccountSuite(t *testing.T) {
+	suite.Run(t, &AccountTestSuite{
+		ctx: context.Background(),
+	})
+}
+
+func (suite *AccountTestSuite) AfterTest(suiteName string, testName string) {
+	// clear accounts table after every test to avoid dependencies and side effects between tests
+	query := `
+		DELETE FROM
+			accounts`
+	_, err := testQueries.db.Exec(suite.ctx, query)
+
+	require.NoError(suite.T(), err)
+}
+
+func (suite *AccountTestSuite) TestCreateAccount() {
 	arg := CreateAccountParams{
 		Owner:    "Max",
 		Balance:  100,
 		Currency: "EUR",
 	}
 
-	account, err := testQueries.CreateAccount(context.Background(), arg)
+	account, err := testQueries.CreateAccount(suite.ctx, arg)
 
-	require.NoError(t, err)
-	require.NotEmpty(t, account)
+	require.NoError(suite.T(), err)
+	require.NotEmpty(suite.T(), account)
 
-	require.Equal(t, arg.Owner, account.Owner)
-	require.Equal(t, arg.Balance, account.Balance)
-	require.Equal(t, arg.Currency, account.Currency)
+	require.Equal(suite.T(), arg.Owner, account.Owner)
+	require.Equal(suite.T(), arg.Balance, account.Balance)
+	require.Equal(suite.T(), arg.Currency, account.Currency)
 
-	require.NotZero(t, account.ID)
-	require.NotZero(t, account.CreatedAt)
+	require.NotZero(suite.T(), account.ID)
+	require.NotZero(suite.T(), account.CreatedAt)
 }
 
-func TestGetAccount(t *testing.T) {
+func (suite *AccountTestSuite) TestGetAccount() {
 	arg := CreateAccountParams{
-		Owner:    "Max2",
-		Balance:  200,
+		Owner:    "Max",
+		Balance:  100,
 		Currency: "EUR",
 	}
 
-	account1, err := testQueries.CreateAccount(context.Background(), arg)
+	account1, err := testQueries.CreateAccount(suite.ctx, arg)
 
-	require.NoError(t, err)
-	require.NotEmpty(t, account1)
+	require.NoError(suite.T(), err)
+	require.NotEmpty(suite.T(), account1)
 
-	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	account2, err := testQueries.GetAccount(suite.ctx, account1.ID)
 
-	require.NoError(t, err)
-	require.NotEmpty(t, account2)
+	require.NoError(suite.T(), err)
+	require.NotEmpty(suite.T(), account2)
 
-	require.Equal(t, account1.ID, account2.ID)
-	require.Equal(t, account1.Owner, account2.Owner)
-	require.Equal(t, account1.Balance, account2.Balance)
-	require.Equal(t, account1.Currency, account2.Currency)
-	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
+	require.Equal(suite.T(), account1.ID, account2.ID)
+	require.Equal(suite.T(), account1.Owner, account2.Owner)
+	require.Equal(suite.T(), account1.Balance, account2.Balance)
+	require.Equal(suite.T(), account1.Currency, account2.Currency)
+	require.WithinDuration(suite.T(), account1.CreatedAt, account2.CreatedAt, time.Second)
+}
+
+func (suite *AccountTestSuite) TestUpdateAccount() {
+	arg := CreateAccountParams{
+		Owner:    "Max",
+		Balance:  100,
+		Currency: "EUR",
+	}
+
+	account1, err := testQueries.CreateAccount(suite.ctx, arg)
+
+	require.NoError(suite.T(), err)
+	require.NotEmpty(suite.T(), account1)
+
+	arg2 := UpdateAccountParams{
+		ID:      account1.ID,
+		Balance: 200,
+	}
+
+	account2, err := testQueries.UpdateAccount(suite.ctx, arg2)
+
+	require.NoError(suite.T(), err)
+	require.NotEmpty(suite.T(), account2)
+
+	require.Equal(suite.T(), account1.ID, account2.ID)
+	require.Equal(suite.T(), account1.Owner, account2.Owner)
+	require.Equal(suite.T(), arg2.Balance, account2.Balance)
+	require.Equal(suite.T(), account1.Currency, account2.Currency)
+	require.WithinDuration(suite.T(), account1.CreatedAt, account2.CreatedAt, time.Second)
 }
