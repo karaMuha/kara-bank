8ea42ec2ab7472cece7b416fde1ed78540362a7d
Muhammed Kara <n.muhammed.kara@gmail.com>
2024-08-11 17:02:27 +0200

    add get account query for transaction

--- cmd/db/repositories/main_test.go
+++ cmd/db/repositories/main_test.go
@@ -6,18 +6,22 @@ import (
 	"log"
 	"os"
 	"testing"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 var testQueries *Queries
+var testDB *pgxpool.Pool
 
 func TestMain(m *testing.M) {
 	testContainer, err := db.CreatePostgresContainer(context.Background())
+	testDB = testContainer.Pool
 
 	if err != nil {
 		log.Fatalf("Could not start test container: %v", err)
 	}
 
-	testQueries = New(testContainer.Pool)
+	testQueries = New(testDB)
 
 	os.Exit(m.Run())
 }
