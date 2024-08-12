af981ca3a764eb282889ab16b3e77ab3c3d6a7bc
Muhammed Kara <n.muhammed.kara@gmail.com>
2024-08-12 15:30:31 +0200

    add transaction tests and refactor account test

--- cmd/db/repositories/main_test.go
+++ cmd/db/repositories/main_test.go
@@ -6,22 +6,21 @@ import (
 	"log"
 	"os"
 	"testing"
-
-	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 var testQueries *Queries
-var testDB *pgxpool.Pool
+var testStore Store
 
 func TestMain(m *testing.M) {
 	testContainer, err := testContainer.CreatePostgresContainer(context.Background())
-	testDB = testContainer.Pool
+	testDB := testContainer.Pool
 
 	if err != nil {
 		log.Fatalf("Could not start test container: %v", err)
 	}
 
 	testQueries = New(testDB)
+	testStore = NewStore(testDB)
 
 	os.Exit(m.Run())
 }
