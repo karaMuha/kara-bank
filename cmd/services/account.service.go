ca14349f6be0483a0f8360f8396d983a450576b6
Muhammed Kara <n.muhammed.kara@gmail.com>
2024-08-14 21:32:26 +0200

    Revert "add ListAccounts"
    This reverts commit 29b6c1b282736329a75b3e9786ab8b3422c42bb9.

--- cmd/services/account.service.go
+++ cmd/services/account.service.go
@@ -20,14 +20,14 @@ func NewAccountService(querier db.Querier) AccountServiceInterface {
 	}
 }
 
-func (a *AccountServiceImpl) CreateAccount(ctx context.Context, arg *dto.CreateAccountDto) (*db.Account, *dto.ResponseError) {
-	params := &db.CreateAccountParams{
-		Owner:    arg.Owner,
-		Currency: arg.Currency,
+func (a *AccountServiceImpl) CreateAccount(ctx context.Context, args *dto.CreateAccountDto) (*db.Account, *dto.ResponseError) {
+	createAccountParams := &db.CreateAccountParams{
+		Owner:    args.Owner,
+		Currency: args.Currency,
 		Balance:  0,
 	}
 
-	createdAccount, err := a.Querier.CreateAccount(ctx, params)
+	createdAccount, err := a.Querier.CreateAccount(ctx, createAccountParams)
 
 	if err != nil {
 		return nil, &dto.ResponseError{
@@ -58,20 +58,6 @@ func (a *AccountServiceImpl) GetAccount(ctx context.Context, id int64) (*db.Acco
 	return account, nil
 }
 
-func (a AccountServiceImpl) ListAccounts(ctx context.Context, arg *dto.ListAccountsDto) ([]*db.Account, *dto.ResponseError) {
-	params := &db.ListAccountsParams{
-		Limit:  arg.Limit,
-		Offset: arg.Offset,
-	}
-
-	accountList, err := a.Querier.ListAccounts(ctx, params)
-
-	if err != nil {
-		return nil, &dto.ResponseError{
-			Message: err.Error(),
-			Status:  http.StatusInternalServerError,
-		}
-	}
-
-	return accountList, nil
+func (a AccountServiceImpl) ListAccounts(ctx context.Context, args *dto.ListAccountsDto) ([]*db.Account, *dto.ResponseError) {
+	return nil, nil
 }
