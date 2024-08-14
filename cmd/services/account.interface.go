4ac6ca757f615374f9441defbf075cb6088f56f1
Muhammed Kara <n.muhammed.kara@gmail.com>
2024-08-14 18:43:06 +0200

    implement create account

--- cmd/services/account.interface.go
+++ cmd/services/account.interface.go
@@ -1,14 +1,15 @@
 package services
 
 import (
+	"context"
 	db "kara-bank/db/repositories"
 	"kara-bank/dto"
 )
 
 type AccountServiceInterface interface {
-	CreateAccount(args *dto.CreateAccountDto) (*db.CreateAccountParams, *dto.ResponseError)
+	CreateAccount(ctx context.Context, args *dto.CreateAccountDto) (*db.Account, *dto.ResponseError)
 
-	GetAccount(id string) (*db.Account, *dto.ResponseError)
+	GetAccount(ctx context.Context, id string) (*db.Account, *dto.ResponseError)
 
-	ListAccounts(args *dto.ListAccountsDto) ([]*db.Account, *dto.ResponseError)
+	ListAccounts(ctx context.Context, args *dto.ListAccountsDto) ([]*db.Account, *dto.ResponseError)
 }
--- cmd/services/account.service.go
+++ cmd/services/account.service.go
@@ -1,8 +1,10 @@
 package services
 
 import (
+	"context"
 	db "kara-bank/db/repositories"
 	"kara-bank/dto"
+	"net/http"
 )
 
 type AccountServiceImpl struct {
@@ -15,14 +17,29 @@ func NewAccountService(querier db.Querier) AccountServiceInterface {
 	}
 }
 
-func (a *AccountServiceImpl) CreateAccount(args *dto.CreateAccountDto) (*db.CreateAccountParams, *dto.ResponseError) {
+func (a *AccountServiceImpl) CreateAccount(ctx context.Context, args *dto.CreateAccountDto) (*db.Account, *dto.ResponseError) {
+	createAccountParams := &db.CreateAccountParams{
+		Owner:    args.Owner,
+		Currency: args.Currency,
+		Balance:  0,
+	}
+
+	createdAccount, err := a.Querier.CreateAccount(ctx, createAccountParams)
+
+	if err != nil {
+		return nil, &dto.ResponseError{
+			Message: err.Error(),
+			Status:  http.StatusInternalServerError,
+		}
+	}
+
+	return createdAccount, nil
+}
+
+func (a *AccountServiceImpl) GetAccount(ctx context.Context, id string) (*db.Account, *dto.ResponseError) {
 	return nil, nil
 }
 
-func (a *AccountServiceImpl) GetAccount(id string) (*db.Account, *dto.ResponseError) {
-	return nil, nil
-}
-
-func (a AccountServiceImpl) ListAccounts(args *dto.ListAccountsDto) ([]*db.Account, *dto.ResponseError) {
+func (a AccountServiceImpl) ListAccounts(ctx context.Context, args *dto.ListAccountsDto) ([]*db.Account, *dto.ResponseError) {
 	return nil, nil
 }
