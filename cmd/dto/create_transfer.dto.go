a0361206eb89279912dd4da95fcc4032890e9e91
Muhammed Kara <n.muhammed.kara@gmail.com>
2024-09-08 16:50:29 +0200

    update readme and dto

--- cmd/dto/create_transfer.dto.go
+++ cmd/dto/create_transfer.dto.go
@@ -2,7 +2,7 @@ package dto
 
 type CreateTransferDto struct {
 	FromUser      string `validate:"required,email"`
-	FromAccountId int64  `validate:"required,min=1"`
-	ToAccountId   int64  `validate:"required,min=1"`
-	Amount        int64  `validate:"required,gt=0"`
+	FromAccountId int64  `json:"from_account_id" validate:"required,min=1"`
+	ToAccountId   int64  `json:"to_account_id" validate:"required,min=1"`
+	Amount        int64  `json:"amount" validate:"required,gt=0"`
 }
