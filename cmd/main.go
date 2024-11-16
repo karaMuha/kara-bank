0dc397e4a83f50dc64aa0bf87d7808427822beb7
Muhammed Kara <n.muhammed.kara@gmail.com>
2024-11-16 21:09:15 +0100

    setup grpc server

--- cmd/main.go
+++ cmd/main.go
@@ -6,15 +6,18 @@ import (
 	"kara-bank/server"
 	"kara-bank/utils"
 	"log"
+	"net"
 	"os"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
 	log.Println("Starting kara-bank")
-	port := os.Getenv("SERVER_PORT")
+	//restPort := os.Getenv("SERVER_PORT")
+	grpcPort := os.Getenv("GRPC_SERVER_PORT")
 
 	log.Println("Initializing token maker")
 	pasetoMaker := utils.NewPasetoMaker("") // TODO: get key for token generation
@@ -23,15 +26,29 @@ func main() {
 	connPool := server.ConnectToDb(context.Background())
 	log.Println("Connected to databse")
 
-	runRestServer(port, connPool, pasetoMaker)
+	// runRestServer(restPort, connPool, pasetoMaker)
+	runGrpcServer(grpcPort, connPool, pasetoMaker)
 }
 
-func runGrpcServer(connPool *pgxpool.Pool, tokenMaker utils.TokenMaker) {
+func runGrpcServer(grpcPort string, connPool *pgxpool.Pool, tokenMaker utils.TokenMaker) {
 	log.Println("Initializing grpc server")
 	handler := server.InitGrpcHandler(connPool, tokenMaker)
 
-	grpc := grpc.NewServer()
-	pb.RegisterKaraBankServer(grpc, handler)
+	server := grpc.NewServer()
+	pb.RegisterKaraBankServer(server, handler)
+	reflection.Register(server)
+	port := "0.0.0.0" + grpcPort
+
+	listener, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatal("cannot create listener")
+	}
+
+	err = server.Serve(listener)
+	if err != nil {
+		log.Fatal("cannot start grpc server")
+	}
+
 }
 
 func runRestServer(port string, connPool *pgxpool.Pool, tokenMaker utils.TokenMaker) {
--- cmd/server/grpc_server.go
+++ cmd/server/grpc_server.go
@@ -1,9 +1,7 @@
 package server
 
 import (
-	db "kara-bank/db/repositories"
 	"kara-bank/pb"
-	"kara-bank/services"
 	"kara-bank/utils"
 
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -17,12 +15,12 @@ type GrpcServer struct {
 
 func InitGrpcHandler(connPool *pgxpool.Pool, tokenMaker utils.TokenMaker) *GrpcServer {
 	// init repository layer
-	store := db.NewStore(connPool)
+	// store := db.NewStore(connPool)
 
 	// init service layer
-	userService := services.NewUserService(store, tokenMaker)
+	/* userService := services.NewUserService(store, tokenMaker)
 	accountService := services.NewAccountService(store)
-	transferService := services.NewTransferService(store)
+	transferService := services.NewTransferService(store) */
 
 	return &GrpcServer{}
 }
